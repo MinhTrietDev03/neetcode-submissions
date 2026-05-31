db8929b63e4068ad72a83352d43bdb7dc76098b1
Le Minh Triet <minhtriet03.dev@gmail.com>
2026-05-31 20:58:32 +0700

    Add: last-stone-weight - submission-31

---
+++ Data Structures & Algorithms/last-stone-weight/submission-31.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        
+        //creaete maxHeap
+        PriorityQueue<Integer> maxHeap =  new PriorityQueue<>((a,b) -> b-a);
+
+        //lặp các phần tử tỏng stones và đưa vào maxHeap
+        for(int stone: stones){
+            maxHeap.offer(stone);
+        }
+
+        //Lặp cho den khi nao con ít nhat 2 vien
+        while(maxHeap.size()>1){
+            
+            //tạo mọt stone1 vá stone 2
+            // lấy phần tử đầu heap và xóa nó ra
+            int stone1 = maxHeap.poll();
+            int stone2 = maxHeap.poll();
+
+            //kiểm tra neeys nếu 2 viên khac nhau
+            if(stone1 != stone2){
+                maxHeap.offer(stone1 -stone2);
+            }
+        }
+
+        // nếu maxHeap rỗng 
+        if(maxHeap.isEmpty()){
+            return 0;
+        }
+
+        return maxHeap.poll();
+
+    }
+}
