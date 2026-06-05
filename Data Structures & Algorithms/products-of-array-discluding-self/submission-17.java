9be0298316c8e5194147933358ca1431553c4891
Le Minh Triet <minhtriet03.dev@gmail.com>
2026-06-05 14:36:27 +0700

    Add: products-of-array-discluding-self - submission-17

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-17.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int[] res = new int[nums.length];
+
+        for(int i=0; i< nums.length ; i++){
+            res[i]= 1;
+        }
+
+        //create variable left
+        int prefix = 1;
+
+        for(int i =0 ; i< nums.length ; i++){
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        //create varialble right
+
+        int postfix = 1;
+
+        for(int i = nums.length -1 ; i>=0; i--){
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+    }
+}  
