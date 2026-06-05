3296af519354917fc29cf7aa9d641b7b08f4e440
Le Minh Triet <minhtriet03.dev@gmail.com>
2026-06-05 15:36:15 +0700

    Add: top-k-elements-in-list - submission-32

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-32.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        //create HashMap 
+        HashMap<Integer , Integer> count = new HashMap<>();
+
+        //Loop và arrange
+        for(int num : nums){
+            count.put(num , count.getOrDefault(num, 0) +1);
+        }
+
+        // create min heap
+        PriorityQueue <Integer> minHeap =  
+        new PriorityQueue<>((a,b) -> count.get(a) -count.get(b) );
+
+        //loop and keySet
+        for(int num : count.keySet()){
+            minHeap.offer(num);
+
+            if(minHeap.size() > k){
+                minHeap.poll();
+            }
+        } 
+
+        //create result
+        int[] result =  new int[k];
+
+        for(int i = k-1 ; i>=0 ; i--){
+            result[i] = minHeap.poll();
+        }
+        return result;
+    }
+}
