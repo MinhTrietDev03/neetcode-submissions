ed197559901a22e9dc7abb0e9952980cef53ec39
Le Minh Triet <minhtriet03.dev@gmail.com>
2026-06-19 14:38:09 +0700

    Add: valid-sudoku - submission-32

---
+++ Data Structures & Algorithms/valid-sudoku/submission-32.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+        //Create three HashSet[9]
+        Set<Character>[] rows =  new HashSet[9];
+        Set<Character>[] cols = new HashSet[9];
+        Set<Character>[] boxes = new HashSet[9];
+
+        //KHỏi tạo Hashset cho từng phần tử 
+        for(int i = 0 ; i< 9 ; i++){
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        //duyệt toàn bộ board 
+        for(int row =0 ; row < 9 ; row ++){
+            for( int col =0 ; col <9 ; col++){
+
+                char num = board[row][col];
+
+                if(num =='.'){
+                    continue;
+                }
+
+                int box = (row / 3)*3 + (col /3);
+
+                if(rows[row].contains(num)){
+                    return false;
+                }
+                if(cols[col].contains(num)){
+                    return false;
+                }
+                if(boxes[box].contains(num)){
+                    return false;
+                }
+
+                rows[row].add(num);
+                cols[col].add(num);
+                boxes[box].add(num);
+            }
+        }
+        return true;
+    }
+}
